abc8891b3ea6e280db20d8dc693a54a539b917a9
MaruthiMohanReddyPutha <19310076+MaruthiMohanReddyPutha@users.noreply.github.com>
2020-07-27 02:12:33 -0400

    Added body parser package

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var dotenv = require('dotenv');
 dotenv.config();
+var bodyParser = require('body-parser');
 
 // --> 7)  Mount the Logger middleware here
 app.use(function(req, res, next) {
@@ -11,7 +12,7 @@ app.use(function(req, res, next) {
 });
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 /** 1) Meet the node console. */
 console.log('Hello World');
